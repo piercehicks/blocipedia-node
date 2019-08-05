0489d262f7a74339e785af25bff077f70688ec67
Pierce Hicks <phicks21@gmail.com>
2019-08-05 11:53:09 -0400

    Module Err

---
+++ src/db/queries.users.js
@@ -0,0 +1,32 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports = {
+
+  createUser(newUser, callback){
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+    return User.create({
+      username: newUser.username,
+      email: newUser.email,
+      password: hashedPassword
+    })
+    .then((user) => {
+      const msg = {
+        to: newUser.email,
+        from: 'info@blocipedia.com',
+        subject: 'Welcome to blocipedia',
+        text: 'Thank you for signing up - start sharing your knowledge with other members in the community!',
+        html: '<strong>WELCOME! GET POSTING!</strong>',
+      };
+      sgMail.send(msg);
+      callback(null, user);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+
+}
