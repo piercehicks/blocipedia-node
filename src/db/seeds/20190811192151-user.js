b7a358f09549d77ddb3b0821232023e7c627dc22
Pierce Hicks <phicks21@gmail.com>
2019-08-11 15:31:19 -0400

    custom db seed

---
+++ src/db/seeds/20190811192151-user.js
@@ -0,0 +1,56 @@
+'use strict';
+const faker = require("faker");
+
+let users = [
+   {
+    username: "blue",
+    email: "blue1@gmail.com",
+    password: "bluebird",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: "standard"
+   },
+   {
+    username: "yellow",
+    email: "yellow2@outlook.com",
+    password: "yellowbird",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: "standard"
+   },
+   {
+    username: "green",
+    email: "green3@gmail.com",
+    password: "greenbird",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: "standard"
+   }
+ ];
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Users", users, {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("Users",null, {});
+
+  }
+};
---
+++ src/db/seeds/20190811192208-wiki.js
@@ -0,0 +1,96 @@
+'use strict';
+const faker = require("faker");
+
+ let wikis = [
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 2
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 3
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 3
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 1
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 2
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 3
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 1
+   },
+   {
+     title: faker.hacker.noun(),
+     body: faker.hacker.phrase(),
+     private: false,
+     createdAt: new Date(),
+     updatedAt: new Date(),
+     userId: 2
+   },
+ ];
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Wikis", wikis, {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("Wikis", null, {});
+
+  }
+};
